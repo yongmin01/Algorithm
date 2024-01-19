c0cbb64abd4b7a6d0617c50e786d81442cd16ea4
yongmin01 <heart77123@naver.com>
2024-01-20 00:33:34 +0900

    일반 수학1_2 : 진법 변환2

--- bj_일반 수학1/bj_11005.py
+++ bj_일반 수학1/bj_11005.py
@@ -1,22 +1,11 @@
 from string import ascii_uppercase
 aList = list(ascii_uppercase)
 N, B = [int(x) for x in input().split()]
-x = 0
-while (N / B**x >= 1) :
-    x += 1
-
-x -= 1
-result = []
-while x >= 0 :
-    result.append(N // B**x)
-    N = N % B**x
-    x -= 1
-
- 
-result2 = []
-
-for x in result :
-    if x > 10 :
-        print(aList[x-10], end="")
+result = ''
+while N != 0 :
+    if (N % B < 10) :
+        result = str(N%B) + result
     else :
-        print(x, end="")
+        result = aList[N%B-10] + result
+    N = N // B
+print(result)
