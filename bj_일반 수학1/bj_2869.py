c13d356096ee1154b845e2afa41962d355d9e334
yongmin01 <heart77123@naver.com>
2024-01-25 01:01:29 +0900

    일반 수학1_7 : 달팽이는 올라가고 싶다

--- bj_일반 수학1/bj_2869.py
+++ bj_일반 수학1/bj_2869.py
@@ -1,17 +1,4 @@
+# 백준 일반 수학1_7 : 달팽이는 올라가고 싶다
+import math
 [A, B, V] = [int(x) for x in input().split()]
-now = 0
-day = 1
-mok = V//(A-B)
-if V % (A-B) == 0 :
-    if (A-B) * (mok-1) + A < V :
-        print("case 1")
-        print(mok)
-    else :
-        while (A-B) * mok < V :
-            mok -= 1
-        print("case 2")
-        print(mok)
-            
-else :
-    print("case 3")
-    print(V//(A-B)+1)
+print(math.ceil((V-B) / (A-B)))
