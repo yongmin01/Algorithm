3a03a8c61ed7fffdaf143f238d0e3ba6033f6cc9
yongmin01 <heart77123@naver.com>
2024-03-05 00:47:30 +0900

    3_분수 합

--- bj_약수, 배수와 소수 2/bj_1735.py
+++ bj_약수, 배수와 소수 2/bj_1735.py
@@ -1,9 +1,11 @@
 # 백준 약수, 배수와 소수 2_3 : 분수 합
 import sys
+
 [A, B] = list(map(int, sys.stdin.readline().split()))
 [C, D] = list(map(int, sys.stdin.readline().split()))
 A = A * D + C * B
 B = B * D
+
 if A >= B :
         MAX = A
         MIN = B
@@ -11,10 +13,12 @@ else :
     MAX = B
     MIN = A
 
+# 최대공약수 구하기
 r = MAX % MIN
+mok = MIN
 while r != 0 : 
-    temp = r
+    mok = r
     r = MIN % r
-    MIN = temp
-    
-print("ㅣㅏ", A // MIN, B // MIN)
+    MIN = mok
+
+print(A // mok, B // mok)
