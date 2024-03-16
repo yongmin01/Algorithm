011d58c6ea83c746fb40c0a157eb24e3bfe345e6
yongmin01 <heart77123@naver.com>
2024-03-16 23:58:19 +0900

    7_카드2

--- bj_스택, 큐, 덱/bj_2164.py
+++ bj_스택, 큐, 덱/bj_2164.py
@@ -1,1 +1,12 @@
 # 백준 스택, 큐, 덱_7 : 카드2
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+queue = deque(list(range(1, N + 1)))
+
+while len(queue) != 1 :
+    del queue[0]
+    queue.append(queue[0])
+    del queue[0]
+print(queue[0])
