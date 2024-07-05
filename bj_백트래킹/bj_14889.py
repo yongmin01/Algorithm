608122fe9b4a4d09a50301ef0258534ff2d0c1ee
yongmin01 <heart77123@naver.com>
2024-07-05 17:24:11 +0900

    8_스타트와 링크

--- bj_백트래킹/bj_14889.py
+++ bj_백트래킹/bj_14889.py
@@ -1,14 +1,17 @@
 # 백준 백트래킹_8 : 스타트와 링크
-# python3로는 실패..
+# python3로는 실패 -> 조합 코드 추가해서 통과
 
 import sys
+from itertools import combinations 
 input = sys.stdin.readline
 
 start_power = 0
 link_power = 0
 min_val = 1e9
+turn = 0
 
 N = int(input())
+com = len(list(combinations([x for x in range(N)], N//2)))
 S = []
 for i in range(N) :
     S.append(list(map(int, input().split())))
@@ -26,7 +29,9 @@ def get_power(team, target, power) :
     return total_power
 
 def solution(idx, start) :
-    if len(start) == N // 2 :
+    global turn
+    if  turn < com/2 and len(start) == N // 2 :
+        turn += 1
         # link 팀원 구하기
         link = []
         for i in range(N) :
