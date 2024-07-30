167c3c4246bd6bb824bbed6e57844a1e98defea2
yongmin01 <heart77123@naver.com>
2024-07-30 16:33:03 +0900

    회의실 배정

---
+++ Morgorithm/week1/bj_1931.py
@@ -0,0 +1,17 @@
+# 백준 : 회의실 배정
+
+N = int(input())
+
+meetings = []
+for i in range(N) :
+    [start, end] = map(int, input().split())
+    meetings.append([start, end])
+
+meetings.sort(key = lambda x: (x[1], x[0]))
+result = 1
+temp = 0
+for i in range(N-1) :
+    if meetings[temp][1] <= meetings[i+1][0] :
+        result += 1
+        temp = i + 1
+print(result)
