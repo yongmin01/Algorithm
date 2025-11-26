99aa6894eb17023baa883ea77e87f42184282ef7
yongmin01 <heart77123@naver.com>
2025-11-26 21:18:27 +0900

    접두사 찾기(Trie)

--- BOJ/Silver/boj_14426.py
+++ BOJ/Silver/boj_14426.py
@@ -29,3 +29,49 @@ for _ in range(m) :
         answer += 1
     
 print(answer)
+
+# Trie 자료구조 사용한 풀이 #
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+class Node (object) :
+    def __init__(self, key, data=None) :
+        self.key = key
+        self.data = data
+        self.children = {}
+
+class Trie (object) :
+    def __init__(self) :
+        self.head = Node(None)
+    
+    def insert(self, string) :
+        curr_node = self.head
+        for char in string :
+            if char not in curr_node.children :
+                curr_node.children[char] = Node(char)
+            curr_node = curr_node.children[char]
+        
+        curr_node.data = string
+    
+    def search(self, string) :
+        curr_node = self.head
+        for char in string :
+            if char in curr_node.children :
+                curr_node = curr_node.children[char]
+            else : return False
+        
+        
+        return True
+    
+trie = Trie()
+for _ in range(n) :
+    trie.insert(input().strip())
+
+answer = 0
+for _ in range(m) :
+    if trie.search(input().strip()) : 
+        answer += 1
+
+print(answer)
